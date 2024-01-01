1d14e8c7e805d904111c66c79ad6116cbfb04b29
minoolian <mw.lee@kakao.com>
2024-01-01 21:51:50 +0900

    feature/1
    close #1
    
    Co-authored-by: swoody1101 <swoody1101@gmail.com>

---
+++ src/test/java/org/example/tdd/TddApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
